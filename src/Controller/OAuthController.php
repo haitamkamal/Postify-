9a6c71b796f6b54ed4225b565552c0c2576f4de5
haitamkamal <169803318+haitamkamal@users.noreply.github.com.>
2025-04-14 12:45:10 +0100

    add: Google and GitHub OAuth authentication support

---
+++ src/Controller/OAuthController.php
@@ -0,0 +1,77 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Members;
+use Doctrine\ORM\EntityManagerInterface;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+class OAuthController extends AbstractController
+{
+    #[Route('/connect/google', name: 'connect_google')]
+    public function connectGoogle(ClientRegistry $clientRegistry)
+    {
+        return $clientRegistry->getClient('google')->redirect();
+    }
+
+    #[Route('/connect/google/check', name: 'connect_google_check')]
+    public function connectGoogleCheck(Request $request, ClientRegistry $clientRegistry, EntityManagerInterface $em)
+    {
+        $client = $clientRegistry->getClient('google');
+        $googleUser = $client->fetchUser();
+
+        $email = $googleUser->getEmail();
+        $user = $em->getRepository(Members::class)->findOneBy(['email' => $email]);
+
+        if (!$user) {
+            $user = new Members();
+            $user->setEmail($email);
+            $user->setUsername($googleUser->getName());
+            $user->setRoles(['ROLE_USER']);
+            $user->setPassword('');
+            $em->persist($user);
+            $em->flush();
+        }
+
+        $token = new UsernamePasswordToken($user, 'main', $user->getRoles());
+        $this->container->get('security.token_storage')->setToken($token);
+
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/connect/github', name: 'connect_github')]
+    public function connectGithub(ClientRegistry $clientRegistry)
+    {
+        return $clientRegistry->getClient('github')
+        ->redirect(['user:email']);
+    }
+
+    #[Route('/connect/github/check', name: 'connect_github_check')]
+    public function connectGithubCheck(Request $request, ClientRegistry $clientRegistry, EntityManagerInterface $em)
+    {
+        $client = $clientRegistry->getClient('github');
+        $githubUser = $client->fetchUser();
+
+        $email = $githubUser->getEmail();
+        $user = $em->getRepository(Members::class)->findOneBy(['email' => $email]);
+
+        if (!$user) {
+            $user = new Members();
+            $user->setEmail($email);
+            $user->setUsername($githubUser->getNickname() ?? $githubUser->getName());
+            $user->setRoles(['ROLE_USER']);
+            $user->setPassword('');
+            $em->persist($user);
+            $em->flush();
+        }
+
+        $token = new UsernamePasswordToken($user, 'main', $user->getRoles());
+        $this->container->get('security.token_storage')->setToken($token);
+
+        return $this->redirectToRoute('app_home');
+    }
+}
