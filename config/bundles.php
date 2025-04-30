c019de2aebd98fc71b440b1a2132de12c366411b
haitamkamal <169803318+haitamkamal@users.noreply.github.com.>
2025-04-30 09:08:39 +0100

    add tailwind

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
